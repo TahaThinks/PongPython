d0c2dde5e43fe866c5ccdbd2e5e68fcd517a55be
TahaThinks <tahaa4587@gmail.com>
2024-02-22 12:30:03 +0400

    Create a Paddle Object from Turtle Class

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-from turtle import Screen
+from turtle import Screen, Turtle
 screen = Screen()
 screen.bgcolor('black')
 screen.setup(width=800, height=600)
 screen.title("Pong by TahaLearns")
+
+
+paddle = Turtle()
+paddle.color("white")
+paddle.shape('square')
+paddle.penup()
+paddle.shapesize(5,1)
+paddle.goto(350, 0)
+
 
 
 screen.exitonclick()
