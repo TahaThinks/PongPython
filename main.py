91a68ee714d4176226bb632a03edc3f5a48366f2
TahaThinks <tahaa4587@gmail.com>
2024-02-22 17:29:25 +0400

    Create 2 Objects of the Paddles

--- main.py
+++ main.py
@@ -6,16 +6,19 @@ screen.setup(width=800, height=600)
 screen.title("Pong by TahaLearns")
 screen.tracer(0)
 
-paddle = Paddle()
+r_position = (350,0)
+l_position = (-350,0)
+
+r_paddle = Paddle(r_position)
+l_paddle = Paddle(l_position)
 
 game_is_on = True
 
 while game_is_on:
     screen.update()
     screen.listen()
-    screen.onkey(paddle.go_up, "Up")
-    screen.onkey(paddle.go_down, "Down")
-
+    screen.onkey(r_paddle.go_up, "Up")
+    screen.onkey(r_paddle.go_down, "Down")
 
 
 screen.exitonclick()
--- paddle.py
+++ paddle.py
@@ -1,14 +1,14 @@
 from turtle import Turtle
 
 class Paddle(Turtle):
-    def __init__(self):
+    def __init__(self, position):
         super().__init__()
         self.paddle = Turtle()
         self.paddle.color("white")
         self.paddle.shape('square')
         self.paddle.penup()
         self.paddle.shapesize(5, 1)
-        self.paddle.goto(350, 0)
+        self.paddle.goto(position)
 
     def go_up(self):
         new_y = self.paddle.ycor() + 20
