7df4ea972e9585100792310818ec0a62fb64dbbe
TahaThinks <tahaa4587@gmail.com>
2024-02-22 12:32:44 +0400

    Listen to KeyStrokes and print the Results

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ screen.bgcolor('black')
 screen.setup(width=800, height=600)
 screen.title("Pong by TahaLearns")
 
+def go_up():
+    print("Up")
+def go_down():
+    print("Down")
 
 paddle = Turtle()
 paddle.color("white")
@@ -12,6 +16,8 @@ paddle.penup()
 paddle.shapesize(5,1)
 paddle.goto(350, 0)
 
-
+screen.listen()
+screen.onkey(go_up, "Up")
+screen.onkey(go_down, "Down")
 
 screen.exitonclick()
